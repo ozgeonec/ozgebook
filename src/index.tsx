97554000ac4cb29eb4a3846351abed495d2a3a5f
Ozge Onec <ozge@asvmalta.com>
2023-06-22 16:24:27 +0200

    esbuild can transform the react code to js

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import {useState, useEffect, useRef} from 'react';
+import * as esbuild from 'esbuild-wasm';
+import {readFile} from "fs";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+
+    const ref = useRef<any>();
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+
+    const startService = async () => {
+
+        ref.current = await esbuild.initialize({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+        ref.current.transform(input,{
+            loader:'jsx'
+        });
+
+    };
+
+    useEffect(() => {
+
+        try {
+            esbuild.build({});
+        } catch (error) {
+            if (error instanceof Error && error.message.includes('initialize')) {
+                esbuild.initialize({
+                    worker: false,
+                    wasmURL: '/esbuild.wasm',
+                });
+            } else {
+                throw error;
+            }
+        }
+    }, []);
+
+    const onClick = () => {
+
+        console.log(ref.current)
+        esbuild
+            .transform(input, {
+                loader: 'jsx',
+                target: 'es2015',
+            })
+            .then((result) => {
+                setCode(result.code);
+                console.log(result)
+            });
+    };
+
+    return <div >
+        <textarea onChange={(e) => setInput(e.target.value)}></textarea>
+        <div>
+            <button onClick={onClick}>
+                Submit
+            </button>
+        </div>
+        <pre>{code}</pre>
+    </div>;
+}
+
+
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App />);
