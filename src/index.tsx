8a6f2b709b3156f1c9e6141764746ccad79bb7a8
Ozge Onec <ozgenuronec@gmail.com>
2023-06-24 22:53:08 +0200

    cleaned

--- src/index.tsx
+++ src/index.tsx
@@ -94,4 +94,4 @@ const App = () => {
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 root.render(<App />);
-// ReactDOM.render(<App />, document.querySelector('#root'));
+
