9f16848421d833737e455d4c2d2af9b213fb5721
Ozge Onec <ozgenuronec@gmail.com>
2023-07-07 17:21:42 +0200

    error handling

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,7 +1,10 @@
 import React, {useEffect, useRef} from 'react';
+import './preview.css';
+
 
 interface PreviewProps {
-    code: string
+    code: string,
+    bundlingStatus: string
 }
 
 
@@ -11,33 +14,50 @@ const html = `
        <body>
        <div id="root"></div>
        <script>
-       window.addEventListener('message',(event)=>{
+       const handleError = (err) => {
+           const root = document.querySelector('#root');
+           root.innerHTML = '<div> <h4>Runtime Error: </h4>' + err + '</div>'
+           console.log(err);
+       }
+       window.addEventListener('error', (event) => {
+           event.preventDefault();
+           handleError(event.error);
+       });
+       window.addEventListener('message',(event) => {
            try {
                 eval(event.data);
            } catch (err) {
-            const root = document.querySelector('#root');
-           root.innerHTML = '<div> <h4>Runtime Error: </h4>' + err + '</div>'
-           console.log(err);
+                handleError(err);
            }
-      
-       },false)
+       },false);
         </script>
         </body>
         </html>
     `;
 
 
-const Preview: React.FC<PreviewProps> = ({code}) => {
+const Preview: React.FC<PreviewProps> = ({code,bundlingStatus}) => {
 
     const iframe = useRef<any>();
 
     useEffect(() => {
         iframe.current.srcdoc = html;
-        iframe.current.contentWindow.postMessage(code, '*');
+        setTimeout(() => {
+            iframe.current.contentWindow.postMessage(code, '*');
+        }, 50);
+
     }, [code]);
 
     return (
-        <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
+        <div className='preview-wrapper'>
+            {bundlingStatus && <div className='preview-error'>{bundlingStatus}</div>}
+            <iframe
+                title="preview"
+                ref={iframe}
+                sandbox="allow-scripts"
+                srcDoc={html}
+            />
+        </div>
     );
 
 };
