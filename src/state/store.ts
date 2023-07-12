2742ab13d003f2a2d99b3ca0c6827c1be6db419a
Ozge Onec <ozgenuronec@gmail.com>
2023-07-12 14:32:35 +0200

    Manual Testing Redux Store

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,5 +1,18 @@
 import {legacy_createStore as createStore, applyMiddleware} from "redux";
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import {ActionType} from "./action-types";
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+
+//Manual Testing Redux Store
+store.dispatch({
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+        id: null,
+        type:'code'
+    }
+});
+
+console.log(store.getState());
